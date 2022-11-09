44e9034707ee89297a116e9d9b99212d6a590359
Marcus Östmo <marcus.ostmo@maptun.com>
2022-11-09 08:43:41 +0100

    Add css purge

---
+++ docs/_app/immutable/chunks/1-86e02311.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-75ea74a4.js";export{t as component};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,8 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.svelte'],
 }
